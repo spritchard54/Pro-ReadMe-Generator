02fbfa3fd2be2f5822c884855341bf4c2ccc4563
spritchard54 <>
2023-12-02 12:21:10 -0500

    fixed html on email field

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 // This is the original code
   function generateMarkdown(data) {
-    return `# ${data.title} \n## Description \n ${data.description} \n## Installation \n ${data.installation} \n## Usage \n ${data.usage} \n## Contributing \n ${data.contributing} \n## Test \n ${data.tests} \n## Questions \n https://github.com/${data.username} \n Email: <a href="mailto:${data.email}>${data.email}</a>`};  
+    return `# ${data.title} \n## Description \n ${data.description} \n## Installation \n ${data.installation} \n## Usage \n ${data.usage} \n## Contributing \n ${data.contributing} \n## Test \n ${data.tests} \n## Questions \n https://github.com/${data.username} \n Email: <a href="mailto:${data.email}">${data.email}</a>`};  
 
 //The code below is an attempt at implicit returns and if it doesn't work revert back to above and try data.title, etc.
 // function generateMarkdown(answers) {
